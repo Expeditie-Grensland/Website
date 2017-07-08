3e566eb33924e418adcf621f8c82dc39aa1722f3
rikmuld <rikmuld@gmail.com>
2017-07-08 23:09:33 +0200

    removed compiled files

--- public/scripts/sockets/socketHandler.js
+++
@@ -1,7 +0,0 @@
-var SocketHandler;
-(function (SocketHandler) {
-    function init() {
-        SocketHandler.socket = io();
-    }
-    SocketHandler.init = init;
-})(SocketHandler || (SocketHandler = {}));
--- server/routes/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Main;
-(function (Main) {
-    function init(app) {
-        app.get("*", (req, res) => res.render("landing"));
-    }
-    Main.init = init;
-})(Main = exports.Main || (exports.Main = {}));
