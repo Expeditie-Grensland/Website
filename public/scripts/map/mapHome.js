2d112709b2b4284715435773509f624d8a7e2745
Maurice Meedendorp <mauricemeedendorp@gmail.com>
2017-10-01 13:02:10 +0900

    Map solution that is not 13MB :D

--- public/scripts/map/mapHome.js
+++ public/scripts/map/mapHome.js
@@ -1,39 +1,9 @@
 var MapHome;
 (function (MapHome) {
     function init() {
-        // const mapBaseLayer = L.tileLayer(
-        //     "https://api.mapbox.com/styles/v1/mauricemeedendorp/cj875zftv39ov2rqiix3hbigz/tiles/256/{z}/{x}/{y}?access_token={access_token}",
-        //     {
-        //         attribution:  "© <a href=\"https://www.mapbox.com/feedback/\">Mapbox</a> © <a href=\"http://www.openstreetmap.org/copyright\">OpenStreetMap</a>",
-        //         access_token: "pk.eyJ1IjoibWF1cmljZW1lZWRlbmRvcnAiLCJhIjoiY2o4NzV5amh5MTVidzJxcWhlbDNhMWlmOCJ9.DvTrMNuuFX3QZZ3boymWPw"
-        //     }
-        // )
-        const map = L.map('mapid').setView([40, 0], 3);
+        //const map = L.map('mapid').setView([40, 0], 3)
         const homecountry = 'Netherlands';
         const countriesBeen = ['Germany', 'Poland', 'Lithuania', 'Latvia', 'Estonia', 'Finland', 'Sweden', 'Norway', 'Denmark'];
-        $.ajax({
-            dataType: "json",
-            url: "/data/countries.geojson",
-            success: function (data) {
-                L.geoJSON(data, {
-                    style: function (feature) {
-                        if (homecountry == feature.properties.name) {
-                            return { color: '#ffee00' };
-                        }
-                        else if (countriesBeen.some(x => x == feature.properties.name)) {
-                            return { color: '#AABBCC' };
-                        }
-                        else {
-                            return { color: '#01aaff' };
-                        }
-                    }
-                }).addTo(map);
-                console.log("GeoJSON loaded!");
-            },
-            error: function (xhr, ajaxOptions, thrownError) {
-                console.log("Error: " + thrownError);
-            }
-        });
     }
     MapHome.init = init;
 })(MapHome || (MapHome = {}));
