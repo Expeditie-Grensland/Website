d13d47861754e60167cad4f4f75505286ef6c9e0
Maurice Meedendorp <mauricemeedendorp@gmail.com>
2017-10-01 21:56:31 +0900

    Kosovo is missing :(

--- public/scripts/map/mapHome.js
+++ public/scripts/map/mapHome.js
@@ -1,10 +1,48 @@
 var MapHome;
 (function (MapHome) {
     function init() {
-        //const map = L.map('mapid').setView([40, 0], 3)
-        const homecountry = 'Netherlands';
-        const countriesBeen = ['Germany', 'Poland', 'Lithuania', 'Latvia', 'Estonia', 'Finland', 'Sweden', 'Norway', 'Denmark'];
+        const svgmap = $('#svgmap');
+        const expedities = MapHome.getExpeditieCountries();
+        let countries = [];
+        for (let expeditie of expedities.expedities) {
+            const name = expeditie.name;
+            for (let country of expeditie.countries) {
+                if (countries.some(x => x == country)) {
+                    svgmap.find('#' + country).css({ fill: 'url(#diagonalHatch)' });
+                }
+                else {
+                    svgmap.find('#' + country).css({ fill: expeditie.color });
+                    countries.push(country);
+                }
+            }
+        }
+        svgmap.find('#' + expedities.homeCountry).css({ fill: '#000FFF' });
     }
     MapHome.init = init;
+    function getExpeditieCountries() {
+        //TODO method stub
+        //TODO should be in proper language
+        return {
+            homeCountry: 'NL',
+            expedities: [
+                {
+                    name: 'Noordkaap',
+                    countries: ['DE', 'PL', 'LT', 'LV', 'EE', 'FI', 'SE', 'NO', 'DK'],
+                    color: '#002ea2' //Suomi blue
+                },
+                {
+                    name: 'Balkan',
+                    countries: ['DE', 'AT', 'SI', 'HR', 'BA', 'ME', 'AL', 'XK', 'MK', 'GR', 'BG', 'RO', 'MD', 'HU', 'SK', 'CZ'],
+                    color: '#e41e20'
+                },
+                {
+                    name: 'Kaukasus',
+                    countries: ['IR', 'AZ', 'GE', 'AM', 'RU', 'BY', 'LT', 'BE'],
+                    color: '#FF7F00'
+                }
+            ]
+        };
+    }
+    MapHome.getExpeditieCountries = getExpeditieCountries;
 })(MapHome || (MapHome = {}));
 MapHome.init();
--- public/scripts/map/mapHome.ts
+++ public/scripts/map/mapHome.ts
@@ -1,13 +1,52 @@
 namespace MapHome {
     export function init() {
 
+        const svgmap = $('#svgmap')
 
+        const expedities  = MapHome.getExpeditieCountries();
 
-        //const map = L.map('mapid').setView([40, 0], 3)
-        const homecountry: string = 'Netherlands'
-        const countriesBeen: string[] = ['Germany', 'Poland', 'Lithuania', 'Latvia', 'Estonia', 'Finland', 'Sweden', 'Norway', 'Denmark']
+        let countries: string[] = []
 
+        for(let expeditie of expedities.expedities) {
+            const name = expeditie.name;
 
+            for(let country of expeditie.countries) {
+                if(countries.some(x => x == country)) {
+                    svgmap.find('#' + country).css({fill: 'url(#diagonalHatch)'})
+                } else {
+                    svgmap.find('#' + country).css({fill: expeditie.color})
+                    countries.push(country)
+                }
+            }
+        }
+
+        svgmap.find('#'+ expedities.homeCountry).css({fill: '#000FFF'})
+    }
+
+    export function getExpeditieCountries() {
+        //TODO method stub
+        //TODO should be in proper language
+
+        return {
+            homeCountry: 'NL',
+            expedities: [
+                {
+                    name: 'Noordkaap',
+                    countries: ['DE', 'PL', 'LT', 'LV', 'EE', 'FI', 'SE', 'NO', 'DK'],
+                    color: '#002ea2' //Suomi blue
+                },
+                {
+                    name: 'Balkan',
+                    countries: ['DE', 'AT', 'SI', 'HR', 'BA', 'ME', 'AL', 'XK', 'MK', 'GR', 'BG', 'RO', 'MD', 'HU', 'SK', 'CZ'],
+                    color: '#e41e20'
+                },
+                {
+                    name: 'Kaukasus',
+                    countries: ['IR', 'AZ', 'GE', 'AM', 'RU', 'BY', 'LT', 'BE'],
+                    color: '#FF7F00'
+                }
+            ]
+        }
     }
 }
 MapHome.init()
