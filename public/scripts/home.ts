761e25cdfed8dd63f5c533a58c3b84cbd4e3d970
Maurice Meedendorp <mauricemeedendorp@gmail.com>
2017-10-17 16:00:34 +0900

    Added slick carousel

--- public/scripts/home.ts
+++ public/scripts/home.ts
@@ -1,3 +1,13 @@
 $(document).ready(function(){
-    $('columns').slick({})
-}
+    $('#columns').slick({
+        dots: true,
+        arrows: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        variableWidth: true,
+        focusOnSelect: true,
+        centerMode: true
+    })
+})
