45f83d41390d001e5a8083b22186af9f2c92f30f
Maurice Meedendorp <mauricemeedendorp@gmail.com>
2017-10-19 11:06:49 +0900

    Fixing server crash

--- server/database/Tables.ts
+++ server/database/Tables.ts
@@ -13,82 +13,6 @@ export namespace TableData {
     }
 
     /**
-     * The expeditie is the wrapping object for all data related to one trip. This is represented on the home page by
-     * one column.
-     */
-    export namespace Expeditie {
-        import RoutePart = TableData.RoutePart.RoutePart
-        export const ID = "Expeditie"
-
-        export const expeditieSchema = new Schema({
-            name: String,
-            year: Number,
-            color: String,
-            background: {
-                image_url: String,
-                position: {
-                    x: Number,
-                    y: Number
-                }
-            },
-            map: {
-                url: String,
-                thumbnail_url: String
-            },
-            movie: {
-                url: String,
-                thumbnail_url: String
-            },
-            routeParts: [reference(TableData.RoutePart.ID)]
-        })
-
-        export interface Expeditie {
-            name: string,
-            year: number,
-            color: string,
-            background?: {
-                image_url: Option<string>,
-                position: Option<{
-                    x: number,
-                    y: number
-                }>
-            },
-            map: {
-                url: Option<string>,
-                thumbnail_url: Option<string>
-            },
-            movie: {
-                url: Option<string>,
-                thumbnail_url: Option<string>
-            },
-            routeParts: string[] | RoutePart[]
-        }
-
-        export interface ExpeditieDocument extends Expeditie, Document {}
-
-        export function expeditie(name: string, year: number, color: string, routeParts: RoutePart[]): Expeditie {
-            return {
-                name: name,
-                year: year,
-                color: color,
-                background: {
-                    image_url: None(),
-                    position: None()
-                },
-                map: {
-                    url: None(),
-                    thumbnail_url: None()
-                },
-                movie: {
-                    url: None(),
-                    thumbnail_url: None()
-                },
-                routeParts: routeParts
-            }
-        }
-    }
-
-    /**
      * A RoutePart is a line describing a set of people's locations. A RoutePart is can only describe the route of a
      * single set of people, so as soon as routes merge or people change
      */
@@ -201,6 +125,82 @@ export namespace TableData {
         export function person(): Person {
             return {
 
+            }
+        }
+    }
+
+    /**
+     * The expeditie is the wrapping object for all data related to one trip. This is represented on the home page by
+     * one column.
+     */
+    export namespace Expeditie {
+        import RoutePart = TableData.RoutePart.RoutePart
+        export const ID = "Expeditie"
+
+        export const expeditieSchema = new Schema({
+            name: String,
+            year: Number,
+            color: String,
+            background: {
+                image_url: String,
+                position: {
+                    x: Number,
+                    y: Number
+                }
+            },
+            map: {
+                url: String,
+                thumbnail_url: String
+            },
+            movie: {
+                url: String,
+                thumbnail_url: String
+            },
+            routeParts: [reference(TableData.RoutePart.ID)]
+        })
+
+        export interface Expeditie {
+            name: string,
+            year: number,
+            color: string,
+            background?: {
+                image_url: Option<string>,
+                position: Option<{
+                    x: number,
+                    y: number
+                }>
+            },
+            map: {
+                url: Option<string>,
+                thumbnail_url: Option<string>
+            },
+            movie: {
+                url: Option<string>,
+                thumbnail_url: Option<string>
+            },
+            routeParts: string[] | RoutePart[]
+        }
+
+        export interface ExpeditieDocument extends Expeditie, Document {}
+
+        export function expeditie(name: string, year: number, color: string, routeParts: RoutePart[]): Expeditie {
+            return {
+                name: name,
+                year: year,
+                color: color,
+                background: {
+                    image_url: None(),
+                    position: None()
+                },
+                map: {
+                    url: None(),
+                    thumbnail_url: None()
+                },
+                movie: {
+                    url: None(),
+                    thumbnail_url: None()
+                },
+                routeParts: routeParts
             }
         }
     }
