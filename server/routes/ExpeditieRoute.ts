4ceb73b533ee7fcaa6d63853e3e3bd362361e811
Maurice Meedendorp <mauricemeedendorp@gmail.com>
2017-11-10 17:37:30 +0900

    Dynamic expeditie route

--- server/routes/ExpeditieRoute.ts
+++ server/routes/ExpeditieRoute.ts
@@ -4,13 +4,17 @@ import {Expeditie} from "../database/Expeditie"
 export namespace ExpeditieRoute {
 
     export function init(app: express.Express) {
-        Expeditie.getExpedities().then((res) => {
-            for (let expeditie of res) {
-                app.get(expeditie.nameShort, (req, res) => {
-                    res.render("expeditie", {
-                        expeditie_name: expeditie.nameShort
+        Expeditie.getExpedities().then((expedities) => {
+            for (let expeditie of expedities) {
+                if(expeditie.showMap) {
+                    console.log("Registering " + expeditie.mapUrl + " as expeditie page.")
+
+                    app.get(expeditie.mapUrl, (req, res) => {
+                        res.render("expeditie", {
+                            expeditie: expeditie
+                        })
                     })
-                })
+                }
             }
         })
     }
