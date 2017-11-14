c2919a266d943b0e8b8b5ac54aac37715dc87291
Maurice Meedendorp <mauricemeedendorp@gmail.com>
2017-11-14 20:31:05 +0900

    Experimenting with map languages

--- public/scripts/expeditie.ts
+++ public/scripts/expeditie.ts
@@ -3,34 +3,20 @@ declare var MapboxLanguage:any
 $(document).ready(() => {
     const map = new mapboxgl.Map({
         container: 'map',
+        //style: 'https://github.com/openmaptiles/klokantech-terrain-gl-style/raw/master/style.json',
         style: 'https://openmaptiles.github.io/klokantech-terrain-gl-style/style-cdn.json',
         center: [5.843570, 52.268337],
         zoom: 6,
     });
-    const language = new MapboxLanguage({
-        defaultLanguage: 'ru'
-    });
+    mapboxgl.setRTLTextPlugin('https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-rtl-text/v0.1.0/mapbox-gl-rtl-text.js', () => {});
 
-    language.setLanguage('https://openmaptiles.github.io/klokantech-terrain-gl-style/style-cdn.json', 'ru')
+    map.on('style.load', () => {
+        console.log("Map style loaded!")
 
-    console.log(language.supportedLanguages)
-
-    map.addControl(language);
-
-
-    map.on('load', () => {
-        console.log("Map loaded!")
-
-        const language = new MapboxLanguage({
-            defaultLanguage: 'ru'
-        });
-
-        language.setLanguage(map.getStyle(), 'ru')
-
-        console.log(language.supportedLanguages)
-
-        map.addControl(language);
-
-        console.log(language)
+        map.setLayoutProperty('country_label-en', 'text-field', '{name:ar}');
+        map.setLayoutProperty('poi_label-en', 'text-field', '{name:ar}');
+        map.setLayoutProperty('road_major_label-en', 'text-field', '{name:ar}');
+        map.setLayoutProperty('place_label_other-en', 'text-field', '{name:ar}');
+        map.setLayoutProperty('place_label_city-en', 'text-field', '{name:ar}');
     })
 })
