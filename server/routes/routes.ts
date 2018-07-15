cafe1c7187a0a56305c28a639f7d9f4a894cbfa2
Martijn Atema <martijn@atema.one>
2018-07-16 01:20:29 +0200

    A sad fix

--- server/routes/routes.ts
+++ server/routes/routes.ts
@@ -4,11 +4,13 @@ import {Debug} from './debug'
 import {ExpeditieRoute} from './expeditieRoute'
 import {Home} from './home'
 import { router as apiRouter } from './api'
+import { router as importStanRouter } from './importStan'
 
 export namespace Routes {
 
     export function init(app: express.Express) {
         app.use('/api', apiRouter);
+        app.use('/import_stan', importStanRouter);
 
         Home.init(app)
 
