d9cf6042fc9df926409937c26b8f613fdd7f6ece
Martijn Atema <martijn@atema.one>
2018-08-16 01:55:19 +0200

    FIXED: Incorrect skip in socket getLocations() There are soooo many locations to load now `:o

--- server-src/sockets/sockets.ts
+++ server-src/sockets/sockets.ts
@@ -1,54 +1,56 @@
-import * as express from 'express'
+import * as express from 'express';
 
-import {Expeditie} from '../database/expeditie'
-import {Route} from '../database/route'
-import {SocketIDs} from './socketHandler'
+import { Expeditie } from '../database/expeditie';
+import { Route } from '../database/route';
+import { SocketIDs } from './socketHandler';
+import { TableData } from '../database/tables';
 
-const sprintf = require('sprintf-js').sprintf
-
+const sprintf = require('sprintf-js').sprintf;
 
 export namespace Sockets {
     export function getRoute(app: express.Express, io: SocketIO.Socket): (expeditieName: string) => void {
         return async name => {
-            const expeditie = await Expeditie.getExpeditieByNameShort(name)
-            const route = await Expeditie.getRoute(expeditie)
+            const expeditie = await Expeditie.getExpeditieByNameShort(name);
+            const route = await Expeditie.getRoute(expeditie);
 
-            io.emit(SocketIDs.GET_ROUTE, name, route)
-        }
+            io.emit(SocketIDs.GET_ROUTE, name, route);
+        };
     }
 
     export function getNodes(app: express.Express, io: SocketIO.Socket): (expeditieName: string) => void {
         return async name => {
-            const expeditie = await Expeditie.getExpeditieByNameShort(name)
-            const route = await Expeditie.getRoute(expeditie)
-            const nodes = await Route.getNodes(route)
+            const expeditie = await Expeditie.getExpeditieByNameShort(name);
+            const route = await Expeditie.getRoute(expeditie);
+            const nodes = await Route.getNodes(route);
 
-            io.emit(SocketIDs.GET_NODES, name, nodes)
-        }
+            io.emit(SocketIDs.GET_NODES, name, nodes);
+        };
     }
 
     export function getLocations(app: express.Express, io: SocketIO.Socket): (expeditieName: string) => void {
         return async name => {
-            const expeditie = await Expeditie.getExpeditieByNameShort(name)
+            const expeditie = await Expeditie.getExpeditieByNameShort(name);
+            const initBatch = 100;
 
-            let batchSize = 100
-            let batch = await Expeditie.getLocationsSortedByVisualArea(expeditie, 0, batchSize)
-            let batchCount = 1
+            let batch: TableData.Location.LocationDocument[];
+            let batchN = 0;
 
-            while (batch.length > 0) {
-                if (!io.connected)
-                    return
+            do {
+                if (!io.connected) return;
 
-                console.log(sprintf("Sending batch %d with %d locations", batchCount, batch.length))
-                io.emit(SocketIDs.GET_LOCATIONS, name, batchCount, batch)
+                let skip = initBatch * (2 ** batchN - 1);
+                let count = initBatch * 2 ** batchN;
 
-                batchCount++
-                batchSize *= 2
-                batch = await Expeditie.getLocationsSortedByVisualArea(expeditie, batchCount * batchSize, batchSize)
-            }
+                batch = await Expeditie.getLocationsSortedByVisualArea(expeditie, skip, count);
 
-            io.emit(SocketIDs.LOCATIONS_DONE, name)
-            console.log('location sending done.')
-        }
+                console.log(sprintf('Sending batch %d with %d locations.', batchN + 1, batch.length));
+                io.emit(SocketIDs.GET_LOCATIONS, name, batchN + 1, batch);
+
+                batchN++;
+            } while (batch.length > 0);
+
+            io.emit(SocketIDs.LOCATIONS_DONE, name);
+            console.log('Location sending done.');
+        };
     }
 }
