e4e95192e97dc55eb7460d1697535426df67ca75
Martijn Atema <martijn@atema.one>
2018-08-16 01:53:16 +0200

    FIXED: Make loadingBar dissapear again

--- public/scripts-src/sockets/sockets.ts
+++ public/scripts-src/sockets/sockets.ts
@@ -1,24 +1,29 @@
 namespace Sockets {
     export function getRoute(expeditieNameShort: string, route: Tables.Route) {
-        LoadingBar.setLoadingText("Received route.")
+        LoadingBar.setLoadingText('Received route.');
 
-        MapHandler.setRoute(route)
+        MapHandler.setRoute(route);
 
-        SocketHandler.requestNodes(expeditieNameShort)
+        SocketHandler.requestNodes(expeditieNameShort);
     }
 
     export function getNodes(expeditieNameShort: string, nodes: Tables.RouteNode[]) {
-        LoadingBar.setLoadingText("Received nodes.")
+        LoadingBar.setLoadingText('Received nodes.');
 
-        MapHandler.addNodes(nodes)
-        SocketHandler.requestLocations(expeditieNameShort)
+        MapHandler.addNodes(nodes);
+
+        SocketHandler.requestLocations(expeditieNameShort);
     }
 
     export function getLocations(expeditieNameShort: string, batchNumber: number, locations: Tables.Location[]) {
-        LoadingBar.setLoadingText("Received location batch " + batchNumber + " with " + locations.length + " locations")
+        LoadingBar.setLoadingText(
+            'Received location batch ' + batchNumber + ' with ' + locations.length + ' locations'
+        );
 
-        MapHandler.addLocations(locations)
+        MapHandler.addLocations(locations);
     }
 
-    export function locationsDone(expeditieNameShort: string) {}
+    export function locationsDone(expeditieNameShort: string) {
+        LoadingBar.setLoadingDone(true);
+    }
 }
