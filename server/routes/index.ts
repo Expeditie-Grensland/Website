b4ffdaa54e0216b8e766a540d887727400313a5b
Martijn Atema <martijn@atema.one>
2018-09-12 13:54:37 +0200

    Moved /login to /members/login (301 redirect)

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -5,7 +5,6 @@ import { router as expeditieRouter } from './expeditie';
 import { router as homeRouter } from './home';
 import { router as apiRouter } from './api';
 import { router as dictionaryRouter } from './dictionary';
-import { router as loginRouter } from './login';
 import { router as membersRouter } from './members';
 
 export function Router(dev: boolean): express.Router {
@@ -14,8 +13,8 @@ export function Router(dev: boolean): express.Router {
     router.use('/api', apiRouter);
     router.use('/dictionary', dictionaryRouter);
     router.use('/woordenboek', dictionaryRouter);
-    router.use('/login', loginRouter);
     router.use('/members', membersRouter);
+    router.get('/login', (req, res) => res.redirect(301, '/members/login'));
 
     if (dev) router.use('/debug', debugRouter);
 
--- server/routes/members/index.ts
+++ server/routes/members/index.ts
@@ -1,6 +1,22 @@
 import * as express from 'express';
+import * as passport from 'passport';
 
 export const router = express.Router();
+
+router.get('/login', (req, res, next) => {
+    if (req.isAuthenticated()) {
+        res.redirect('/members');
+    } else {
+        next();
+    }
+}, (req, res) => {
+    res.render('members/login');
+});
+
+router.post('/login', passport.authenticate('ldapauth', {
+    successRedirect: '/members',
+    failureRedirect: '/members/login'
+}));
 
 router.get('/logout', (req, res) => {
     req.logout();
