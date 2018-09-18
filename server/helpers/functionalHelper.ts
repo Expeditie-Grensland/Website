7140d5b5421e82d160d4532bb6125e038b245a63
Martijn Atema <martijn@atema.one>
2018-09-18 19:31:06 +0200

    Add types to functionalHelper.ts

--- server/helpers/functionalHelper.ts
+++ server/helpers/functionalHelper.ts
@@ -1,15 +1,15 @@
-export const pipe = (...fns) =>
-    input =>
+export const pipe = (...fns: ((value: any) => any)[]): any =>
+    (input: any): any =>
         fns.reduce((chain, fn) => (...args) => fn(chain(...args)), input);
 
-export const compose = (...fns) =>
-    input =>
+export const compose = (...fns: ((value: any) => any)[]) =>
+    (input: any): any =>
         fns.reduceRight((chain, fn) => (...args) => fn(chain(...args)), input);
 
-export const aPipe = (...fns) =>
-    input =>
+export const aPipe = (...fns: ((value: any) => any)[]) =>
+    (input: any): Promise<any> =>
         fns.reduce((chain, fn) => chain.then(fn), Promise.resolve(input));
 
-export const aCompose = (...fns) =>
-    input =>
+export const aCompose = (...fns: ((value: any) => any)[]) =>
+    (input: any): Promise<any> =>
         fns.reduceRight((chain, fn) => chain.then(fn), Promise.resolve(input));
