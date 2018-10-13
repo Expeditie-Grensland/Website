44cbc852a9e08bcf83562163c922c65b47dd81de
Martijn Atema <martijn@atema.one>
2018-10-13 17:05:29 +0200

    Serve unfinished user's expedities on /app-backend/expedities

--- src/server/appBackend/index.ts
+++ src/server/appBackend/index.ts
@@ -3,7 +3,8 @@ import * as passport from 'passport';
 
 import { AuthHelper } from '../helpers/authHelper';
 import { People } from '../components/people';
-import { PersonOrID } from '../components/people/model';
+import { PersonDocument } from '../components/people/model';
+import { Expedities } from '../components/expedities';
 
 export const router = express.Router();
 
@@ -12,17 +13,17 @@ router.get('/', (req, res) => {
 });
 
 router.post('/authenticate', (req, res, next) => {
-    passport.authenticate('ldapauth', { session: false }, (err: Error | null, user: PersonOrID | null, info: Error | null) => {
+    passport.authenticate('ldapauth', { session: false }, (err: Error | null, user: PersonDocument | null, info: Error | null) => {
         if (err)
             next(err);
         else if (!user)
-            next([401, info]);
+            next(info);
         else
             AuthHelper.generateJwt(user, (err2, token) => {
                 if (err2)
                     next(err2);
                 else
-                    res.status(200).json({ token });
+                    res.status(200).json({ token, name: user.name });
             });
     })(req, res, next);
 });
@@ -31,21 +32,33 @@ router.use((req, res, next) => {
     if (req.headers.authorization && req.headers.authorization.split(' ')[0] == 'Bearer')
         AuthHelper.parseJwt(req.headers.authorization.split(' ')[1], (err, decoded: string | { id?: string }) => {
             if (err)
-                next([401, err.message]);
+                next(err);
             else {
                 if (typeof decoded === 'string' || decoded.id == undefined)
-                    return next([401, new Error('Unexpected jwt format')]);
+                    return next(new Error('Unexpected jwt format'));
                 People.getById(decoded.id)
                     .then(person => {
                         req.user = person;
                         next();
                     })
-                    .catch(err2 => next([401, err2]));
+                    .catch(next);
             }
         });
     else
-        next([401, new Error('Token header not present')]);
+        next(new Error('Token header not present'));
 });
+
+router.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
+    if (res.headersSent) {
+        return next(err);
+    }
+
+    res.status(401).json({ message: err.message });
+});
+
+router.get('/expedities', (req, res) =>
+    Expedities.getUnfinishedByParticipant(req.user, { name: 1, nameShort: 1, sequenceNumber: 1 })
+        .then(x => res.status(200).json(x)));
 
 // TODO: router.use other files
 
@@ -57,11 +70,6 @@ router.use((err: any, req: express.Request, res: express.Response, next: express
         return next(err);
     }
 
-    let status = 500;
-
-    if (Array.isArray(err))
-        [status, err] = err;
-
-    res.status(status).json({ message: err.message });
+    res.status(500).json({ message: err.message });
 });
 
