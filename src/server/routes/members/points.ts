3145124ba2be1378b5206cc85534b0b349d4d083
Martijn Atema <martijn@atema.one>
2019-01-30 10:49:58 +0100

    Parse database earned points in right format

--- src/server/routes/members/points.ts
+++ src/server/routes/members/points.ts
@@ -1,7 +1,30 @@
 import * as express from 'express';
+import * as R from 'ramda';
+
+import { EarnedPoints } from '../../components/earnedPoints';
+import { ExpeditieDocument } from '../../components/expedities/model';
+import { EarnedPointDocument } from '../../components/earnedPoints/model';
+import { OffsetDates } from '../../components/offsetDate';
+import { PersonDocument } from '../../components/people/model';
 
 export const router = express.Router();
 
 router.get('/', async (req, res) => {
-    res.render('members/points');
+    const earnedPoints = R.pipe(
+        R.map((x: EarnedPointDocument) => {
+            return {
+                date: OffsetDates.getDateDDMM(x.date),
+                amount: x.amount,
+                name: (<PersonDocument>x.personId).name,
+                team: (<PersonDocument>x.personId).team,
+                expeditie: x.expeditieId ? (<ExpeditieDocument>x.expeditieId).name : ''
+            };
+        }),
+        // @ts-ignore
+        R.groupWith(R.eqProps('expeditie'))
+    )(await EarnedPoints.getAllPopulated());
+
+    console.log(earnedPoints);
+
+    res.render('members/points', { earnedPoints });
 });
