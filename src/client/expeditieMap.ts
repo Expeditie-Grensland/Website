162182b9f944b8e1dabc12ee440a271cdba1e553
mgmeedendorp <mauricemeedendorp@gmail.com>
2021-02-17 16:26:51 +0100

    Added mapbox terrain

--- src/client/expeditieMap.ts
+++ src/client/expeditieMap.ts
@@ -76,6 +76,27 @@ const setRoute = (res: GeoJsonResult) => {
 
 
 map.on('load', () => {
+    map.addSource('mapbox-dem', {
+        'type': 'raster-dem',
+        'url': 'mapbox://mapbox.mapbox-terrain-dem-v1',
+        'tileSize': 512,
+        'maxzoom': 14
+    });
+    // add the DEM source as a terrain layer with exaggerated height
+    map.setTerrain({ 'source': 'mapbox-dem', 'exaggeration': 1.25 });
+
+    // add a sky layer that will show when the map is highly pitched
+    map.addLayer({
+        'id': 'sky',
+        'type': 'sky',
+        'paint': {
+            'sky-type': 'atmosphere',
+            'sky-atmosphere-sun': [0.0, 0.0],
+            'sky-atmosphere-sun-intensity': 15
+        }
+    });
+
+
     console.info('Map load!');
     worker.postMessage(['styleLoaded']);
 });
