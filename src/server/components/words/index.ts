2b96f7ed2259e64c762d214832f7576a96e0443e
Martijn Atema <martijn@atema.one>
2023-07-25 23:10:47 +0200

    Opschonen words/index.ts

--- src/server/components/words/index.ts
+++ src/server/components/words/index.ts
@@ -1,28 +1,16 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-import { Word, WordDocument, WordModel, WordOrId } from './model.js';
-import * as Documents from '../documents/index.js';
+import { Word, WordDocument, WordModel } from "./model.js";
 
 export const create = (word: Word): Promise<WordDocument> =>
-    WordModel.create(word);
+  WordModel.create(word);
 
 export const getAll = (): Promise<WordDocument[]> =>
-    WordModel
-        .find({})
-        .collation({ locale: 'nl', strength: 1 })
-        .sort({ word: 1 })
-        .exec();
+  WordModel.find({})
+    .collation({ locale: "nl", strength: 1 })
+    .sort({ word: 1 })
+    .exec();
 
-export const getById = (id: mongoose.Types.ObjectId): Promise<WordDocument | null> =>
-    WordModel
-        .findById(id)
-        .exec();
-
-export const getByIds = (ids: mongoose.Types.ObjectId[]): Promise<WordDocument[]> =>
-    WordModel
-        .find({ _id: { $in: ids } })
-        .exec();
-
-export const getDocument = (word: WordOrId): Promise<WordDocument | null> =>
-    Documents.getDocument(getById)(word);
-
+export const getById = (
+  id: mongoose.Types.ObjectId
+): Promise<WordDocument | null> => WordModel.findById(id).exec();
