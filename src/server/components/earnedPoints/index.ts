bed72be49a88e2cc4b1d2c969598ffb724a5e81d
Martijn Atema <martijn@atema.one>
2023-07-25 23:03:59 +0200

    Opschonen earnedPoints/index.ts

--- src/server/components/earnedPoints/index.ts
+++ src/server/components/earnedPoints/index.ts
@@ -1,11 +1,8 @@
-import { EarnedPoint, EarnedPointDocument, EarnedPointModel } from './model.js';
+import { EarnedPointDocument, EarnedPointModel } from './model.js';
 import mongoose from 'mongoose';
 
 export const getById = (id: mongoose.Types.ObjectId): Promise<EarnedPointDocument | null> =>
     EarnedPointModel.findById(id).exec();
-
-export const create = (earnedPoint: EarnedPoint): Promise<EarnedPointDocument> =>
-    EarnedPointModel.create(earnedPoint);
 
 export const getAll = (): Promise<EarnedPointDocument[]> =>
     EarnedPointModel.find().sort({ 'dateTime.stamp': -1 }).exec();
