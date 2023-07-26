8eaa5cd4e7f069ac74dd4a16382e6fa0bab2f62d
Martijn Atema <martijn@atema.one>
2023-07-26 20:56:09 +0200

    Refactor geonodes model met infer

--- src/server/components/geoNodes/model.ts
+++ src/server/components/geoNodes/model.ts
@@ -1,51 +1,39 @@
-import mongoose from 'mongoose';
-import { GeoNodeId } from './id.js';
-import { DocumentOrId } from '../documents/index.js';
-import { ExpeditieId } from '../expedities/id.js';
-import { PersonId } from '../people/id.js';
+import mongoose, { InferSchemaType, Schema } from "mongoose";
 
-// FIXME: replace ObjectId with DocumentOrId<> types (in all models) OR union types as return type for functions
-export interface GeoNode {
-    _id?: mongoose.Types.ObjectId;
-    expeditieId: mongoose.Types.ObjectId;
-    personIds: mongoose.Types.ObjectId[];
-    timeFrom?: number;
-    timeTill?: number;
-}
+import { ExpeditieId } from "../expedities/id.js";
+import { PersonId } from "../people/id.js";
+import { GeoNodeId } from "./id.js";
 
-export interface GeoNodeDocument extends GeoNode, mongoose.Document {
-    _id: mongoose.Types.ObjectId;
-    timeFrom: number;
-    timeTill: number;
-}
-
-const geoNodeSchema = new mongoose.Schema({
-    expeditieId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: ExpeditieId,
-        required: true
+const geoNodeSchema = new Schema({
+  expeditieId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: ExpeditieId,
+    required: true,
+  },
+  personIds: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: PersonId,
+      required: true,
     },
-    personIds: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: PersonId,
-        required: true
-    }],
-    timeFrom: {
-        type: Number,
-        default: 0,
-        set: (t: number) => t > 1e10 ? t / 1000 : t
-    },
-    timeTill: {
-        type: Number,
-        default: Number.POSITIVE_INFINITY,
-        set: (t: number) => t > 1e10 ? t / 1000 : t
-    }
-})
-    .index({
-        expeditieId: 1,
-        timeTill: 1
-    });
+  ],
+  timeFrom: {
+    type: Number,
+    default: 0,
+    set: (t: number) => (t > 1e10 ? t / 1000 : t),
+  },
+  timeTill: {
+    type: Number,
+    default: Number.POSITIVE_INFINITY,
+    set: (t: number) => (t > 1e10 ? t / 1000 : t),
+  },
+});
 
-export const geoNodeModel = mongoose.model<GeoNodeDocument>(GeoNodeId, geoNodeSchema);
+geoNodeSchema.index({
+  expeditieId: 1,
+  timeTill: 1,
+});
 
-export type GeoNodeOrId = DocumentOrId<GeoNodeDocument>;
+export type GeoNode = InferSchemaType<typeof geoNodeSchema>;
+
+export const geoNodeModel = mongoose.model(GeoNodeId, geoNodeSchema);
