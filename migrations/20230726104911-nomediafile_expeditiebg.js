1496b259739bff5481d065ff6818f468b442143b
Martijn Atema <martijn@atema.one>
2023-07-26 14:22:03 +0200

    Gebruik transacties in migraties

--- migrations/20230726104911-nomediafile_expeditiebg.js
+++ migrations/20230726104911-nomediafile_expeditiebg.js
@@ -1,33 +1,41 @@
 import mongo from "mongodb";
 
-export const up = async (db) => {
-  const cursor = await db.collection("expedities").find();
+export const up = async (db, client) => {
+  const session = client.startSession();
 
-  for await (const doc of cursor) {
-    const bg = doc.backgroundFile;
-    const newBg = `${bg.id.toString()}.${bg.ext}`;
+  await session.withTransaction(async () => {
+    const cursor = await db.collection("expedities").find();
 
-    await db
-      .collection("expedities")
-      .updateOne({ _id: doc._id }, { $set: { backgroundFile: newBg } });
-  }
+    for await (const doc of cursor) {
+      const bg = doc.backgroundFile;
+      const newBg = `${bg.id.toString()}.${bg.ext}`;
+
+      await db
+        .collection("expedities")
+        .updateOne({ _id: doc._id }, { $set: { backgroundFile: newBg } });
+    }
+  });
 };
 
-export const down = async (db) => {
-  const cursor = await db.collection("expedities").find();
+export const down = async (db, client) => {
+  const session = client.startSession();
 
-  for await (const doc of cursor) {
-    const [name, ext] = doc.backgroundFile.split(".");
+  await session.withTransaction(async () => {
+    const cursor = await db.collection("expedities").find();
 
-    const newBg = {
-      id: new mongo.ObjectId(name),
-      ext,
-      mime: "image/jpeg",
-      restricted: false,
-    };
+    for await (const doc of cursor) {
+      const [name, ext] = doc.backgroundFile.split(".");
 
-    await db
-      .collection("expedities")
-      .updateOne({ _id: doc._id }, { $set: { backgroundFile: newBg } });
-  }
+      const newBg = {
+        id: new mongo.ObjectId(name),
+        ext,
+        mime: "image/jpeg",
+        restricted: false,
+      };
+
+      await db
+        .collection("expedities")
+        .updateOne({ _id: doc._id }, { $set: { backgroundFile: newBg } });
+    }
+  });
 };
