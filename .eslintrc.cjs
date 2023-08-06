fe647ebbabc07ccf4dbb764b5e35b7361d38390f
Martijn Atema <martijn@atema.one>
2023-08-06 20:32:45 +0200

    Herstructureer eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,22 @@
 module.exports = {
   root: true,
-  ignorePatterns: ['.eslintrc.cjs', 'dist/'],
+  ignorePatterns: [".eslintrc.cjs", "dist/"],
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/stylistic',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/stylistic",
   ],
-  plugins: ['@typescript-eslint'],
-  parser: '@typescript-eslint/parser',
+  plugins: ["@typescript-eslint"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ["tsconfig.json"],
+  },
   env: {
     node: true,
   },
   rules: {
-    'no-constant-condition': ['error', { checkLoops: false }],
-    '@typescript-eslint/consistent-type-definitions': 'off',
+    "no-constant-condition": ["error", { checkLoops: false }],
+    "@typescript-eslint/consistent-type-definitions": "off",
   },
 };
