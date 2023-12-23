2068209e2921891fd7687050922193e6ceaac149
Martijn Atema <martijn@atema.one>
2023-12-23 20:41:39 +0100

    Simple cli met inquirer; nog geen functie

---
+++ src/server/cli/files.ts
@@ -0,0 +1,88 @@
+import inquirer from "inquirer";
+import { getAllExpedities } from "../components/expedities/index.js";
+import { stat, constants, access } from "node:fs/promises";
+
+export default async function filesCli() {
+  const answers = await inquirer.prompt([
+    {
+      name: "type",
+      message: "Type bestand",
+      type: "list",
+      choices: [
+        { name: "Expeditie-film", value: "film" },
+        { name: "Expeditie-achtergrond", value: "achtergrond" },
+        {
+          name: "Afbeelding-bijlage (voor verhaal, woord of citaat)",
+          value: "afbeelding",
+        },
+        {
+          name: "Video-bijlage (voor verhaal, woord of citaat)",
+          value: "video",
+        },
+        {
+          name: "Audio-bijlage (voor verhaal, woord of citaat)",
+          value: "audio",
+        },
+      ],
+    },
+    {
+      name: "bijlageVan",
+      message: "Bijlage van",
+      when: ({ type }) =>
+        type === "afbeelding" || type === "video" || type === "audio",
+      type: "list",
+      choices: [
+        { name: "Verhaal", value: "verhaal" },
+        { name: "Woord", value: "woord" },
+        { name: "Citaat", value: "citaat" },
+      ],
+    },
+    {
+      name: "expeditie",
+      message: "Expeditie",
+      when: ({ type, bijlageVan }) =>
+        type === "film" || type === "achtergrond" || bijlageVan === "verhaal",
+      type: "list",
+      loop: false,
+      choices: async () =>
+        (
+          await getAllExpedities()
+        ).map((exp) => ({ name: exp.name, value: exp.nameShort })),
+    },
+    {
+      name: "beschrijving",
+      message: "Bestandsnaam (beknopte beschrijving)",
+      when: ({ type }) =>
+        type === "afbeelding" || type === "video" || type === "audio",
+      type: "input",
+      transformer: (input: string, { bijlageVan, expeditie }) =>
+        bijlageVan === "verhaal"
+          ? `${expeditie}-verhaal-${input}`
+          : bijlageVan === "woord"
+          ? `woord-${input}`
+          : bijlageVan === "citaat"
+          ? `citaat-${input}`
+          : input,
+      validate: (input: string) =>
+        /^([a-z0-9]+-)*[a-z0-9]+$/.test(input) ||
+        "Moet bestaan uit a-z en 0-9, gescheiden met -",
+    },
+    {
+      name: "bron",
+      message: "Bronbestand",
+      type: "file-tree-selection",
+      enableGoUpperDirectory: true,
+      validate: async (input: string) => {
+        try {
+          await access(input, constants.R_OK);
+          return (await stat(input)).isFile();
+        } catch (err) {
+          return false;
+        }
+      },
+    },
+  ]);
+
+  console.dir(answers);
+  return;
+}
---
+++ src/server/cli/main.ts
@@ -0,0 +1,21 @@
+import inquirer from "inquirer";
+import inquirerFileTreeSelection from "inquirer-file-tree-selection-prompt";
+import mongoose from "mongoose";
+import filesCli from "./files.js";
+import { config } from "../helpers/configHelper.js";
+
+console.info("Connecting to database...");
+await mongoose.connect(config.EG_MONGO_URL);
+
+inquirer.registerPrompt("file-tree-selection", inquirerFileTreeSelection);
+
+const answers = await inquirer.prompt({
+  name: "commando",
+  message: "Actie",
+  type: "list",
+  choices: [{ name: "Bestand converteren en uploaden", value: "files" }],
+});
+
+if (answers.commando === "files") await filesCli();
+
+await mongoose.disconnect();
