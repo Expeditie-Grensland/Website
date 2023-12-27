96ad5b2791cdf9441d4b0bc983a19df4d68aae8b
Martijn Atema <martijn@atema.one>
2023-12-27 17:43:40 +0100

    Refactor afbeelding converters

--- src/server/files/types/achtergrond.ts
+++ src/server/files/types/achtergrond.ts
@@ -1,41 +1,7 @@
 import { Converter } from "../convert.js";
-import {
-  ImageVersion,
-  convertWithMagick,
-  transformImgToJpeg,
-  transformImgToWebp,
-  transformImgWidth,
-} from "./imageWithMagick.js";
-
-const versions: ImageVersion[] = [
-  {
-    name: "volledig.jpg",
-    transformers: [transformImgToJpeg],
-  },
-  {
-    name: "normaal.jpg",
-    transformers: [transformImgToJpeg, transformImgWidth("1500>")],
-  },
-  {
-    name: "normaal.webp",
-    transformers: [transformImgToWebp, transformImgWidth("1500>")],
-  },
-  {
-    name: "klein.jpg",
-    transformers: [transformImgToJpeg, transformImgWidth("500>")],
-  },
-  {
-    name: "klein.webp",
-    transformers: [transformImgToWebp, transformImgWidth("500>")],
-  },
-  {
-    name: "miniscuul.jpg",
-    transformers: [transformImgToJpeg, transformImgWidth("30")],
-  },
-];
+import { convertAfbeelding } from "./afbeelding.js";
 
 export const convertAchtergrond: Converter = {
   extension: "achtergrond",
-  convert: (inputFile, outputDir) =>
-    convertWithMagick(inputFile, outputDir, versions),
+  convert: convertAfbeelding.convert,
 };
--- src/server/files/types/afbeelding.ts
+++ src/server/files/types/afbeelding.ts
@@ -1,28 +1,83 @@
+import gm from "gm";
+import path from "node:path";
 import { Converter } from "../convert.js";
-import {
-  ImageVersion,
-  convertWithMagick,
-  transformImgToJpeg,
-  transformImgToWebp,
-  transformImgWidth,
-} from "./imageWithMagick.js";
+
+const magick = gm.subClass({ imageMagick: "7+" });
+
+export type ImageVersion = {
+  name: string;
+  format: "jpg" | "webp";
+  quality?: number;
+  width?: number;
+};
+
+const transformVersion = (imgVersion: ImageVersion, inputImg: gm.State) => {
+  let img = inputImg
+    .setFormat(imgVersion.format)
+    .colorspace("sRGB")
+    .strip()
+    .quality(imgVersion.quality || 80);
+
+  if (imgVersion.format === "jpg") img = img.samplingFactor(2, 2);
+
+  if (imgVersion.width)
+    img = img.thumbnail(imgVersion.width, undefined as unknown as number, ">");
+
+  return img;
+};
+
+export const convertWithMagick = async (
+  inputFile: string,
+  outputDir: string,
+  imgVersions: ImageVersion[]
+) => {
+  for (const imgVersion of imgVersions) {
+    await new Promise<void>((resolve, reject) => {
+      transformVersion(imgVersion, magick(inputFile)).write(
+        path.join(outputDir, `${imgVersion.name}.${imgVersion.format}`),
+        (err) => {
+          if (err) reject(err);
+          else resolve();
+        }
+      );
+    });
+  }
+};
 
 const versions: ImageVersion[] = [
   {
-    name: "volledig.jpg",
-    transformers: [transformImgToJpeg],
+    name: "origineel",
+    format: "jpg",
+    quality: 100,
   },
   {
-    name: "normaal.jpg",
-    transformers: [transformImgToJpeg, transformImgWidth("1500>")],
+    name: "volledig",
+    format: "jpg",
   },
   {
-    name: "normaal.webp",
-    transformers: [transformImgToWebp, transformImgWidth("1500>")],
+    name: "normaal",
+    format: "jpg",
+    width: 1500,
   },
   {
-    name: "miniscuul.jpg",
-    transformers: [transformImgToJpeg, transformImgWidth("30")],
+    name: "normaal",
+    format: "webp",
+    width: 1500,
+  },
+  {
+    name: "klein",
+    format: "jpg",
+    width: 500,
+  },
+  {
+    name: "klein",
+    format: "webp",
+    width: 500,
+  },
+  {
+    name: "miniscuul",
+    format: "jpg",
+    width: 30,
   },
 ];
 
