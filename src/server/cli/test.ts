b6b60848e54931d033a2bd3e6017936e662ef03b
Martijn Atema <martijn@atema.one>
2024-01-02 13:16:12 +0100

    Achtergebleven test bestand verwijderen

--- src/server/cli/test.ts
+++
@@ -1,8 +0,0 @@
-import { convertAndUploadFile } from "../files/convert.js";
-import { convertAchtergrond } from "../files/types/achtergrond.js";
-
-await convertAndUploadFile(
-  "test-file",
-  "/home/martijn/Test/input.jpg",
-  convertAchtergrond
-);
