9d3bc79cbf21f93cce23b93e12689afc1ebea86c
Martijn Atema <martijn@atema.one>
2024-03-31 23:57:09 +0200

    Tsc server zonder gulp

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -13,11 +13,6 @@ gulp.task('copy:dev', copyTask(gulp, { dev: true }));
 gulp.task('copy:prod', copyTask(gulp, { prod: true }));
 gulp.task('copy:watch', copyTask(gulp, { watch: true }));
 
-import serverTask from './gulp-files/server.mjs';
-gulp.task('server:dev', serverTask(gulp, { dev: true }));
-gulp.task('server:prod', serverTask(gulp, { prod: true }));
-gulp.task('server:watch', serverTask(gulp, { watch: true }));
-
 import stylesTask from './gulp-files/styles.mjs';
 gulp.task('styles:dev', stylesTask(gulp, { dev: true }));
 gulp.task('styles:prod', stylesTask(gulp, { prod: true }));
@@ -34,22 +29,14 @@ gulp.task('errorpages:watch', errorPagesTask(gulp, { watch: true }));
  */
 
 gulp.task(
-  'build:dev',
-  gulp.parallel('client:dev', 'copy:dev', 'server:dev', 'styles:dev', 'errorpages:dev')
-);
-
-gulp.task(
   'build:prod',
-  gulp.series(
-    gulp.parallel('client:prod', 'styles:prod', 'server:prod'),
-    gulp.parallel('copy:prod', 'errorpages:prod')
-  )
+  gulp.series('client:prod', 'styles:prod', 'copy:prod', 'errorpages:prod')
 );
 
 gulp.task(
   'watch',
   gulp.series(
-    gulp.parallel('client:watch', 'copy:dev', 'server:dev', 'styles:dev', 'errorpages:dev'),
-    gulp.parallel('copy:watch', 'server:watch', 'styles:watch', 'errorpages:watch')
+    gulp.parallel('client:watch', 'copy:dev', 'styles:dev', 'errorpages:dev'),
+    gulp.parallel('copy:watch', 'styles:watch', 'errorpages:watch')
   )
 );
