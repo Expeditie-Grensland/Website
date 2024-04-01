dc94afadfbfb63c98e49c79e85596150a2fb4c37
Martijn Atema <martijn@atema.one>
2024-04-01 12:33:26 +0200

    Script voor clean

---
+++ scripts/clean.js
@@ -0,0 +1,10 @@
+import { rm } from "node:fs/promises";
+
+const opts = ["dev", "dist"];
+const dir = process.argv[2];
+
+if (!opts.includes(dir))
+  throw new Error(`Invalid choice ${dir} (choose from: ${opts.join(", ")}})`);
+
+await rm(`${dir}/`, { recursive: true, force: true });
+console.error(`Deleted ${dir}/`);
