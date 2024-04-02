68d22aef40e84c52886512d4c6b63856e7f75bab
Martijn Atema <martijn@atema.one>
2024-04-02 19:17:47 +0200

    Gebruik esbuild ook voor server

---
+++ build/server.js
@@ -0,0 +1,27 @@
+import { build, context } from "esbuild";
+import { globby } from "globby";
+
+const opts = ["dev", "dist"];
+const dir = process.argv[2];
+
+if (!opts.includes(dir))
+  throw new Error(`Invalid choice ${dir} (choose from: ${opts.join(", ")}})`);
+
+const isProd = dir === "dist";
+
+const files = await globby(["src/server/**/*.ts", "!src/server/**/*.d.ts"]);
+
+const options = {
+  entryPoints: files,
+  outdir: `${dir}/server/`,
+  platform: "node",
+  target: "node20",
+  logLevel: "info",
+};
+
+if (isProd) {
+  await build({ ...options });
+} else {
+  const ctx = await context({ ...options, sourcemap: true });
+  await ctx.watch();
+}
