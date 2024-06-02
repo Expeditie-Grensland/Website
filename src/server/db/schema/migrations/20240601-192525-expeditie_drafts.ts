f35069a90d1ef18216cbca8adbdef8d7d5dbefd6
Martijn Atema <martijn@atema.one>
2024-06-02 19:47:59 +0200

    Fix concepten migratie

--- src/server/db/schema/migrations/20240601-192525-expeditie_drafts.ts
+++ src/server/db/schema/migrations/20240601-192525-expeditie_drafts.ts
@@ -7,11 +7,17 @@ export const up = async (db: Kysely<any>) => {
     .addColumn("draft", "boolean", (col) => col.notNull().defaultTo(false))
     .execute();
 
+  await db
+    .updateTable("expeditie")
+    .set({ draft: true })
+    .where(sql`background_file <> ''`, "is not", true)
+    .execute();
+
   await db.schema
     .alterTable("expeditie")
     .addCheckConstraint(
       "expeditie_draft_background_check",
-      sql`draft OR background_file IS NOT NULL`
+      sql`draft OR background_file <> ''`
     )
     .execute();
 };
