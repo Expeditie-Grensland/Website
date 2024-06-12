2980032281870190b3d423ef78535ef4e941e600
Martijn Atema <martijn@atema.one>
2024-06-12 21:26:46 +0200

    Gebruik hash-wasm voor bcrypt & herschrijf pbkdf2

--- src/server/db/person.ts
+++ src/server/db/person.ts
@@ -1,10 +1,16 @@
+import { pbkdf2, randomBytes } from "crypto";
+import { bcrypt, bcryptVerify } from "hash-wasm";
 import db from "./schema/database.js";
-import { compare, hash } from "bcrypt";
-import { pbkdf2Sync } from "crypto";
 
-const hashPassword = (password: string) => hash(password, 12);
-const checkPassword = (password: string, storedHash: string) =>
-  compare(password, storedHash);
+const hashPassword = async (password: string) =>
+  await bcrypt({
+    password,
+    salt: randomBytes(16),
+    costFactor: 12,
+  });
+
+const checkPassword = async (password: string, hash: string) =>
+  await bcryptVerify({ password, hash });
 
 export const getAllPersons = () =>
   db.selectFrom("person").selectAll().execute();
@@ -12,25 +18,33 @@ export const getAllPersons = () =>
 export const getPerson = (id: string) =>
   db.selectFrom("person").where("id", "=", id).selectAll().executeTakeFirst();
 
+const pbkdf2HashRegex =
+  /^\{PBKDF2-(?<digest>.*)\}(?<iterations>\d+)\$(?<salt>.+)\$(?<key>.+)$/;
+
+const checkPbkdf2Password = (password: string, hash: string) =>
+  new Promise<boolean | null>((resolve, reject) => {
+    const params = pbkdf2HashRegex.exec(hash)?.groups;
+    if (!params) return resolve(null);
+
+    const digest = params.digest.toLowerCase();
+    const iterations = parseInt(params.iterations);
+    const salt = Buffer.from(params.salt, "base64");
+    const key = Buffer.from(params.key, "base64");
+
+    pbkdf2(password, salt, iterations, key.length, digest, (err, calcKey) => {
+      if (err) return reject(err);
+
+      resolve(key.equals(calcKey));
+    });
+  });
+
 export const authenticatePerson = async (id: string, password: string) => {
   const user = await getPerson(id);
   if (!user || !user.password) return null;
 
-  const pbkdfParams =
-    /^\{PBKDF2-(?<digest>.*)\}(?<iterations>\d+)\$(?<salt>.+)\$(?<key>.+)$/.exec(
-      user.password
-    )?.groups;
+  const pbkdf2Check = await checkPbkdf2Password(password, user.password);
 
-  if (pbkdfParams) {
-    const digest = pbkdfParams.digest.toLowerCase();
-    const iterations = parseInt(pbkdfParams.iterations);
-    const salt = Buffer.from(pbkdfParams.salt, "base64");
-    const key = Buffer.from(pbkdfParams.key, "base64");
-
-    const calcKey = pbkdf2Sync(password, salt, iterations, key.length, digest);
-
-    if (!calcKey.equals(key)) return null;
-
+  if (pbkdf2Check) {
     return await db
       .updateTable("person")
       .set({ password: await hashPassword(password) })
@@ -39,6 +53,6 @@ export const authenticatePerson = async (id: string, password: string) => {
       .executeTakeFirst();
   }
 
-  if (await checkPassword(password, user.password)) return user;
-  else return null;
+  if (pbkdf2Check === false) return null;
+  return (await checkPassword(password, user.password)) ? user : null;
 };
